a02a9133638ea3a887eb64569f17d786bb9cf3d8
Peter Lee <pil206@nyu.edu>
2020-02-13 19:11:39 -0500

    first commit

---
+++ PowerupManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    private bool doublePoints;
+    private bool safeMode;
+
+    private bool powerupActive;
+
+    private float powerupLengthCounter;
+
+    private ScoreManager theScoreManager;
+    private PlatformGenerator thePlatformGenerator;
+    private GameManager theGameManager;
+
+    private float normalPointsPerSecond;
+    private float spikeRate;
+
+    private PlatformDestroyer[] spikeList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theScoreManager = FindObjectOfType<ScoreManager>();
+        thePlatformGenerator = FindObjectOfType<PlatformGenerator>();
+        theGameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(powerupActive)
+        {
+            powerupLengthCounter -= Time.deltaTime;
+
+            if(theGameManager.powerupReset)
+            {
+                powerupLengthCounter = 0;
+                theGameManager.powerupReset = false;
+            }
+
+            if(doublePoints)
+            {
+                theScoreManager.pointsPerSecond = normalPointsPerSecond * 2f;
+                theScoreManager.shouldDouble = true;
+            }
+
+            if(safeMode)
+            {
+                thePlatformGenerator.randomSpikeThreshold = 0;
+            }
+
+
+            if(powerupLengthCounter <= 0)
+            {
+                theScoreManager.pointsPerSecond = normalPointsPerSecond;
+                thePlatformGenerator.randomSpikeThreshold = spikeRate;
+
+                powerupActive = false;
+            }
+        }
+    }
+
+    public void ActivatePowerup(bool points, bool safe, float time)
+    {
+        doublePoints = points;
+        safeMode = safe;
+        powerupLengthCounter = time;
+
+        if(safeMode)
+        {
+            spikeList = FindObjectsOfType<PlatformDestroyer>();
+        for(int i = 0; i < spikeList.Length; i++)
+        {
+            if(spikeList[i].gameObject.name.Contains("spikes"))
+            {
+                spikeList[i].gameObject.SetActive(false);
+            }
+        }
+        }
+        normalPointsPerSecond = theScoreManager.pointsPerSecond;
+        spikeRate = thePlatformGenerator.randomSpikeThreshold;
+
+        powerupActive = true;
+    }
+}
